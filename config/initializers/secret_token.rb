9ed6de646b47fe2559cf63635f4f556119e009b0
Andrew Taber <andrew.e.taber@gmail.com>
2013-06-29 15:26:28 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orpheo::Application.config.secret_token = '31bdd8808e2570b6382e74b4a18ceb767b62ba059c576c078252c3a84be05eccecd9ec84983a5014171e660b5b4f2675731328907035bdbf69da05223900e6e9'
